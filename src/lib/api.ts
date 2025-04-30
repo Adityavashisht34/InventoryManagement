d49e880223447c7ef7436c97003707581b5e4084
Aditya Vashisht <112467903+Adityavashisht34@users.noreply.github.com>
2025-04-30 10:29:29 +0530

    Update api.ts

--- src/lib/api.ts
+++ src/lib/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:4000/api';
+const API_URL = 'https://inventorymanagementbackend-nck1.onrender.com/api';
 
 const api = {
   setToken: (token: string) => {
